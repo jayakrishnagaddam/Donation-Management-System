ddee6e58d23ba6bc16367bcdd6bde4327a6d6549
Jaya Krishna Gaddam <2100090162@kluniversity.in>
2024-03-01 10:35:22 +0530

    Added Emergencies in DB and Shown in Donation page

--- app.py
+++ app.py
@@ -1,28 +1,29 @@
 from flask import Flask, render_template
 from flask_pymongo import PyMongo
-import os
 
-app=Flask(__name__)
-app.config['Secret Key']="1234"
-app.config['MONGO_URI']="mongodb+srv://2100090162:manigaddam@deepsheild.kzgpo9p.mongodb.net/deepsheild"
-database=PyMongo(app)
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "1234"
+app.config['MONGO_URI'] = "mongodb+srv://2100090162:manigaddam@deepsheild.kzgpo9p.mongodb.net/Donations"
+
+mongo = PyMongo(app)
 
 @app.route("/home")
 def home():
-  return render_template('home.html')
+    return render_template('home.html')
   
 @app.route("/")
 def function():
-  return render_template('index.html')
+    return render_template('index.html')
 
-@app.route('/donatepage', method=['post','get'])
+@app.route('/donatepage')
 def donatepage():
-  return render_template('donatepage.html')
+   
+    emergencies_data = mongo.db.Emergency.find()
+    return render_template('donatepage.html', Emergency=emergencies_data)
 
 @app.route('/contactus')
 def contact():
-  return render_template('contactus.html')
-
+    return render_template('contactus.html')
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=True)
