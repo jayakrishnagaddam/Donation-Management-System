01773222d1740fc689b20b0f7da253cc6a11bbde
Jaya Krishna Gaddam <2100090162@kluniversity.in>
2024-03-01 08:41:32 +0530

    Backend Connection

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template
+from flask_pymongo import PyMongo
+import os
 
-app = Flask(__name__)
-
-
+app=Flask(__name__)
+app.config['Secret Key']="1234"
+app.config['MONGO_URI']="mongodb+srv://2100090162:manigaddam@deepsheild.kzgpo9p.mongodb.net/deepsheild"
+database=PyMongo(app)
 
 @app.route("/home")
 def home():
@@ -12,6 +15,13 @@ def home():
 def function():
   return render_template('index.html')
 
+@app.route('/donatepage')
+def donatepage():
+  return render_template('donatepage.html')
+
+@app.route('/contactus')
+def contact():
+  return render_template('contactus.html')
 
 
 if __name__ == '__main__':
