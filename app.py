9119cafe0b1bf5e6fbe504bdad6ef7926562f9ce
Jaya Krishna Gaddam <2100090162@kluniversity.in>
2024-03-02 00:28:21 +0530

    Added a needs page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
@@ -10,20 +10,43 @@ mongo = PyMongo(app)
 @app.route("/home")
 def home():
     return render_template('home.html')
-  
+
 @app.route("/")
 def function():
     return render_template('index.html')
 
 @app.route('/donatepage')
 def donatepage():
-   
     emergencies_data = mongo.db.Emergency.find()
     return render_template('donatepage.html', Emergency=emergencies_data)
 
 @app.route('/contactus')
 def contact():
     return render_template('contactus.html')
+
+
+@app.route('/Needs', methods=['POST','GET'])
+def Needs():
+    if request.method == 'POST':
+        requirement_name = request.form['Requirement_name']
+        patient_name = request.form['Patient_name']
+        organization_location = request.form['Location']
+        contact_number = request.form['Contact_number']
+        proof_of_requirement = request.form['Proof']
+        
+        # Save proof_of_requirement to a folder or cloud storage and get its URL
+        
+        # Insert the data into MongoDB
+        mongo.db.Needs.insert_one({
+            'requirement_name': requirement_name,
+            'patient_name': patient_name,
+            'organization_location': organization_location,
+            'contact_number': contact_number,
+            'proof_of_requirement': proof_of_requirement  # You can replace this with the URL
+        })
+        return redirect(url_for('donatepage'))
+        
+    return render_template('Needs.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
